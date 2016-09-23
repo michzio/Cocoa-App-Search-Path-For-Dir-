a6e142c07f10cd640b6d8e3007242c6b02650508
michzio <michzio@hotmail.com>
2016-09-23 14:25:11 +0200

    Initial Commit

---
+++ search_path_for_dir/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  search_path_for_dir
+//
+//  Created by Michal Ziobro on 23/09/2016.
+//  Copyright © 2016 Michal Ziobro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
